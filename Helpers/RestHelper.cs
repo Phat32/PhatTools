922cc518e0294cdbd3e360b2c2dc1ef97286160e
tom <tom@TDESK>
2023-04-25 01:45:41 -0700

    Add project files.

---
+++ Helpers/RestHelper.cs
@@ -0,0 +1,80 @@
+﻿using RestSharp;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhatRestHelper.Helpers
+{
+    public static class RestHelper
+    {
+        public static async Task<bool> ExecuteAsync(RestClient client, RestRequest request)
+        {
+            try
+            {
+                var response = await client.ExecuteAsync(request).ConfigureAwait(false);
+                return response.StatusCode == HttpStatusCode.OK ? true : LogError<bool>(response);
+            }
+            catch (Exception ex)
+            {
+                return LogError<bool>(ex);
+            }
+        }
+
+        public static async Task<T> ExecuteAsync<T>(RestClient client, RestRequest request)
+        {
+            try
+            {
+                var response = await client.ExecuteAsync<T>(request).ConfigureAwait(false);
+               return response.StatusCode == HttpStatusCode.OK ? response.Data : LogError<T>(response);
+            }
+            catch (Exception ex)
+            {
+                return LogError<T>(ex);
+            }
+        }
+
+        public static async Task<string> ExecuteContentAsync(RestClient client, RestRequest request)
+        {
+            try
+            {
+                var response = await client.ExecuteAsync<string>(request).ConfigureAwait(false);
+                return response.StatusCode == HttpStatusCode.OK ? response.Content : LogError<string>(response);
+            }
+            catch (Exception ex)
+            {
+                return LogError<string>(ex);
+            }
+        }
+
+        public static T LogError<T>(RestResponse response)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                
+            }            
+
+            return default(T);
+        }
+
+        public static T LogError<T>(Exception ex)
+        {
+            try
+            {
+                
+
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+            return default(T);
+        }
+    }
+}
---
+++ Managers/RestManager.cs
@@ -0,0 +1,85 @@
+﻿using PhatRestHelper.Helpers;
+using RestSharp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhatRestHelper
+{
+    public class RestManager
+    {
+        public RestManager(string uri)
+        {
+            _uri = uri;
+        }
+
+        private string _uri;
+
+        private RestClient _client;
+
+        private RestClient Client
+        {
+            get
+            {
+                if (_client == null)
+                    _client = new RestClient(_uri);
+                return _client;
+            }
+        }
+
+        public Task<object> Get(string endPoint, IEnumerable<KeyValuePair<string, string>> args = null)
+        {
+            var test = new KeyValuePair<string, string> ( "test", "test 2" );
+
+            var request = new RestRequest(endPoint, Method.Get);
+            if (args?.Any() == true)
+            {
+                foreach (var arg in args)
+                {
+                    request.AddParameter(arg.Key, arg.Value);
+                }
+            }
+
+            return RestHelper.ExecuteAsync<object>(Client, request);
+        }
+
+        public async Task<object> Post(string endPoint, object obj, IEnumerable<KeyValuePair<string, string>> args = null)
+        {
+            var request = new RestRequest(endPoint, Method.Post);
+            if (args?.Any() == true)
+            {
+                foreach (var arg in args)
+                {
+                    request.AddParameter(arg.Key, arg.Value);
+                }
+            }
+            request.AddJsonBody(obj);
+
+            return await RestHelper.ExecuteAsync<object>(Client, request).ConfigureAwait(false);
+        }
+
+        public async Task<object> Put(string endPoint, object obj)
+        {
+            var request = new RestRequest(endPoint, Method.Put);
+            request.AddJsonBody(obj);
+
+            return await RestHelper.ExecuteAsync<object>(Client, request).ConfigureAwait(false);
+        }
+
+        public async Task<object> DeleteFromBody(string endPoint, object obj)
+        {
+            var request = new RestRequest(endPoint, Method.Delete);
+            request.AddJsonBody(obj);
+
+            return await RestHelper.ExecuteAsync<object>(Client, request).ConfigureAwait(false);
+        }
+
+        public async Task<object> DeleteFromParam(string endPoint, KeyValuePair<string, string> args)
+        {
+            var request = new RestRequest(endPoint, Method.Delete);
+            request.AddParameter(args.Key, args.Value);
+
+            return await RestHelper.ExecuteAsync<object>(Client, request).ConfigureAwait(false);
+        }
+    }
+}
